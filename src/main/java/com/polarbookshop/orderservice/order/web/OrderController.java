a5a9c042a300b293153626abc4b2337316150be5
Sean Maxwell <sa.maxwell@yahoo.com>
2023-04-12 22:59:08 -0500

    Added reactive REST API

---
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderController.java
@@ -0,0 +1,30 @@
+package com.polarbookshop.orderservice.order.web;
+
+import com.polarbookshop.orderservice.order.domain.Order;
+import com.polarbookshop.orderservice.order.domain.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrders( @RequestBody @Valid OrderRequest orderRequest ) {
+        return orderService
+                .submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
+}
